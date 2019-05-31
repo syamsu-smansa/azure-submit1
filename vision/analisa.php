80e3809ade0d1576314e3055bad7a85b0f1af20b
syamsu-smansa <syamsu.smansa.polban@gmail.com>
2019-05-31 18:32:16 +0700

    Update analisa.php

--- vision/analisa.php
+++ vision/analisa.php
@@ -1,1 +1,84 @@
-<?php
+<html>
+
+<head>
+  <title>Analisa Gambar</title>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+</head> 
+<body>
+<script type="text/javascript">  
+  <script type="text/javascript">
+    function processImage() {
+        // **********************************************
+        // *** Update or verify the following values. ***
+        // **********************************************
+ 
+        // Replace <Subscription Key> with your valid subscription key.
+        var subscriptionKey = "768f0870691249afb0f272957d37488a";
+ 
+        // You must use the same Azure region in your REST API method as you used to
+        // get your subscription keys. For example, if you got your subscription keys
+        // from the West US region, replace "westcentralus" in the URL
+        // below with "westus".
+        //
+        // Free trial subscription keys are generated in the "westus" region.
+        // If you use a free trial subscription key, you shouldn't need to change
+        // this region.
+        var uriBase = "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+ 
+        // Request parameters.
+        var params = {
+            "visualFeatures": "Categories,Description,Color",
+            "details": "",
+            "language": "en",
+        };
+ 
+        // Display the image.
+        var sourceImageUrl = $("#inputImage").src;
+//        document.querySelector("#sourceImage").src = sourceImageUrl;
+ 
+        // Make the REST API call.
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+ 
+            // Request headers.
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                xhrObj.setRequestHeader(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+ 
+            type: "POST",
+ 
+            // Request body.
+            data: '{"url": ' + '"' + sourceImageUrl + '"}',
+        })
+ 
+        .done(function(data) {
+            // Show formatted JSON on webpage.
+            $("#info").val(JSON.stringify(data, null, 2));
+        })
+ 
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ? "Error. " :
+                errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ? "" :
+                jQuery.parseJSON(jqXHR.responseText).message;
+            alert(errorString);
+        });
+    };
+</script>
+</script>  
+<table>
+ <tr><td>
+   <img src="<?php echo $_GET['url'] ?>" id="inputImage" /><br/>
+   <button onclick="processImage()">Analisa</button>
+ </td></tr>  
+<tr><td><h3>Hasil Analisa</h3></td></tr>
+<tr><td>Captions</td><tr>
+<tr><td><span id="captions">&nbsp;</span></td></tr>
+<tr><td>Info Lengkap</td><tr>
+<tr><td><span id="info">&nbsp;</span></td></tr>
+</table>
+</body>
+</html>
