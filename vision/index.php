9d557819d61295ad01851a49d39cc237b5c6b1a7
syamsu-smansa <syamsu.smansa.polban@gmail.com>
2019-05-31 17:37:23 +0700

    Update index.php

--- vision/index.php
+++ vision/index.php
@@ -216,7 +216,7 @@ try {
 	}
 ?>
 </tr>
-<tr><td><a href="index.php?Cleanup&containerName">Hapus storage</a></td></tr>
+<tr><td><a href="index.php?Cleanup=1&containerName=<?php echo $containerName; ?>>">Hapus storage</a></td></tr>
 <tr><td><form>Isi text dari gambar yg sudah di upload di atas: <input type="text" /><button type="button">Analisa</button></form>
 <tr><td><h3>Hasil Analisa</h3></td></tr>
 <tr><td><img src="" id="hasil" /></td></td>
