25b49f84be6ac018bf66bd6eddb1efa28fdf9bff
syamsu-smansa <syamsu.smansa.polban@gmail.com>
2019-05-31 17:43:57 +0700

    Update index.php

--- vision/index.php
+++ vision/index.php
@@ -69,7 +69,7 @@ $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
 $createContainerOptions->addMetaData("key1", "gambar");
 $createContainerOptions->addMetaData("key2", "analisa");
 
-$containerName = "blockblobs112232";
+$containerName = "blockblobs1321";
 
 try {
 	// Create container.
